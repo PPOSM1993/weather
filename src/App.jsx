80b450ac7f81fff118882963062f86882d1c7b73
pedroosorio <pedro.osorio04@inacapmail.cl>
2023-11-07 18:20:53 -0300

    commit for weather-app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import WheatherApp from "./components/WeatherApp/WheatherApp"
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <WheatherApp />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
